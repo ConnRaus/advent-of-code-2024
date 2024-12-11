3959c9a8b651fc3610663ec77b24c44d2c3635e1
Connor Rauscher <connor.rauscher@gmail.com>
2024-12-11 00:06:09 -0700

    add day 11 solution

---
+++ day11/part1.py
@@ -0,0 +1,37 @@
+# WARNING: Scary non-DP code that only works for a depth of like 35 below!!!
+
+# Get single line input
+def parseInput(filename):
+    with open(filename, 'r') as file:
+        inputText = file.read()
+    return inputText.strip().split()
+
+
+def blink(lst):
+    # If stone has number 0, replace with stone with number 1.
+    # If stone has a number with even number of digits, it is replaced by two stones. First half of the digits on left stone, second half of digits on right stone. 
+        # (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
+    # else stone is stone multiplied by 2024
+    newLst = []
+    for ele in lst:
+        if ele == '0':
+            newLst.append('1')
+        elif len(ele)%2==0:
+            firstHalf = ele[0:int(len(ele)/2)]
+            secondHalf = ele[int(len(ele)/2):len(ele)]
+            newLst.append(str(int(firstHalf)))
+            newLst.append(str(int(secondHalf)))
+        else: 
+            newLst.append(str(int(ele)*2024))
+    return newLst
+
+
+numArray = parseInput('input.txt')
+for i in range(0,25):
+    print("Blink #", i+1)
+    numArray = blink(numArray)
+
+print(len(numArray))
+
+
+
---
+++ day11/part2.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+# Get single line input
+def parseInput(filename):
+    with open(filename, 'r') as file:
+        inputText = file.read()
+    lst = [int(x) for x in inputText.strip().split()]
+    return lst
+
+# memoize everything!!!
+dpList = {}
+
+def recursiveBlink(n, depth=0):
+    if depth == 75:
+        return 1
+    
+    if (n, depth) in dpList:
+        return dpList[(n, depth)]
+        
+    if n == 0:
+        # was just returning 1 for the longest time instead of recursing here... :(
+        result = recursiveBlink(1, depth+1)
+    elif len(str(n))%2==0:
+        # yeah its ugly but i wanted integers and part 1 code was easy to just change to int and use n
+        firstHalf = int(str(n)[0:int(len(str(n))/2)])
+        secondHalf = int(str(n)[int(len(str(n))/2):len(str(n))]) 
+        result = recursiveBlink(firstHalf, depth+1) + recursiveBlink(secondHalf, depth+1)
+    else: 
+        result = recursiveBlink(n*2024, depth+1)
+
+    dpList[(n, depth)] = result
+    return result
+
+
+numArray = parseInput('input.txt')
+
+
+totalSum = 0
+for n in numArray:
+    totalSum += recursiveBlink(n, 0)
+
+print(totalSum)
+
+
+
